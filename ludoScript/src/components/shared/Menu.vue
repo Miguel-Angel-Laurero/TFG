4b68f7f1c3c103af70c28674b13cc73f68d5bfdd
Miguel-Angel-Laurero <malaurero@alu.fpsanantonio.com>
2026-03-16 14:27:47 +0100

    fix: ajustes del menu

--- ludoScript/src/components/shared/Menu.vue
+++ ludoScript/src/components/shared/Menu.vue
@@ -1,16 +1,19 @@
 <template> 
-    <RouterLink to="/" class="border-b border-gray-100 bg-indigo-700 px-6 py-2 mx-2 rounded-xl">Inicio</RouterLink>
-    <RouterLink to="/login-view" v-if="!is_logged" class="border-b border-gray-100 bg-indigo-700 px-6 py-2 mx-2 rounded-xl">Iniciar sesión</RouterLink>
-    <RouterLink to="/profile-view/" class="border-b border-gray-100 bg-indigo-700 px-6 py-2 rounded-xl">Perfil</RouterLink>
+    <div class="flex">
+        <RouterLink to="/" class="border-b border-gray-100 bg-indigo-700 text-gray-50 font-bold px-6 py-2 mx-2 rounded-xl">Inicio</RouterLink>
+        <!-- añadir exclamacion al principio del v-if cuando el login sea funcional con datos que comprobar -->
+        <RouterLink v-if="auth.isLogged" to="/login-view"  class="border-b border-gray-100 bg-indigo-700 text-gray-50 font-bold px-6 py-2 mx-2 rounded-xl">Iniciar sesión</RouterLink>
+        <template v-else>
+            <RouterLink to="/login-view"  class="border-b border-gray-100 bg-indigo-700 text-gray-50 font-bold px-6 py-2 mx-2 rounded-xl" @click="cerrarSesion">Cerrar sesión</RouterLink>
+            <RouterLink to="/profile-view/" class="border-b border-gray-100 bg-indigo-700 text-gray-50 font-bold px-6 py-2 rounded-xl">Perfil</RouterLink>
+        </template>
+    </div>
 </template>
 <script setup>
-    import { defineProps } from 'vue';
-    const props = defineProps({
-        titulo: {
-            type: String,
-            default: 'Título'
-        }
-    })
+import { useAuthStore } from '@/stores/auth.store'
+const auth = useAuthStore()
 
-    const is_logged = false;
+function cerrarSesion() {
+  auth.logout()
+}
 </script>
