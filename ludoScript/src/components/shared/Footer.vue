51cc7ba4761ce96bf5727ddbbcdc9f1e46f26374
Miguel-Angel-Laurero <malaurero@alu.fpsanantonio.com>
2026-03-23 10:05:26 +0100

    fix: footer responsive

--- ludoScript/src/components/shared/Footer.vue
+++ ludoScript/src/components/shared/Footer.vue
@@ -1,9 +1,9 @@
 <template>
-    <div class="w-full bg-indigo-900 text-gray-50/50 bottom-0 h-20  align-middle px-6 ">
+    <div class="w-full bg-indigo-900 text-gray-50/50 bottom-0 px-6 py-4">
         <p><span class="font-righteous">LudoScript </span>- Aprende a programar jugando. Retos, puntos y logros que harán tu aprendizaje adictivo.</p>
-        <div class="w-full flex border-t-1 mt-2 justify-between ">
-            <p class="flex items-center ">&copy; 2026 LudoScript - Todos los derechos reservados </p>
-            <div class="py-2">
+        <div class="w-full flex border-t-1 mt-2 justify-between">
+            <p class="flex items-center text-xs">&copy; 2026 LudoScript - Todos los derechos reservados </p>
+            <div class="flex flex-wrap justify-center sm:justify-end items-center gap-x-4 gap-y-2 text-xs">
                 <span class="px-4">Privacidad</span>
                 <span class="px-4">Terminos de uso</span>
                 <span class="px-4">Cookies</span>
